6163e8c39d563d2fce2c04d6970dc1bce0977c2b
Wind-Works <learningrivenleague@outlook.com>
2022-11-22 23:42:23 -0500

    more updates

--- script.js
+++ script.js
@@ -44,6 +44,10 @@ $cards
     var bgX_pos = 10 + (((lp - 50)/1.5) * 0.6);
     var bgY_pos = 48.6 - (((tp - 50)/2) * -0.32);
 
+    //nebula
+    var nX_pos = 50 + (((lp - 50)/1.5) * 0.6);
+    var nY_pos = 11.5 - (((tp - 50)/2) * -0.32);
+
     //border
     var border_X = (-tx*0.15) - 50;
     var border_Y = (ty*0.08) - 50;
@@ -53,17 +57,24 @@ $cards
     var sprk_pos = `background-position: ${px_spark}% ${py_spark}%;`
     //var axis_pos = `transform-origin: ${bgX_pos}% ${bgY_pos}%;`
     var baxis_pos = `top:${bgY_pos}%; left:${bgX_pos}%;`
+    var naxis_pos = `top:${nY_pos}%; left:${nX_pos}%;`
 
     //var opc = `opacity: ${p_opc/100};`
     var tf = ` transform: rotateX(${ty*-1}deg) rotateY(${tx*-1}deg)`
     var tf_border = `transform: translate(${border_X}%, ${border_Y}%) rotateX(${ty*-1}deg) rotateY(${tx*-1}deg);`
+    var hue_rotate = `filter: hue-rotate(${pa}deg) saturate(2) opacity: 0.8;`
+    var brightness = 1.6 + (100 - py)/100;
+    var gems_glow = `filter: brightness(${brightness}) saturate(2) hue-rotate(${pa}deg);`
     // need to use a <style> tag for psuedo elements
     //${opc}
     var style = `
       .card:hover:before { ${grad_pos} }  /* gradient */
       .card:hover:after { ${sprk_pos} }   /* sparkles */ 
-      .card:hover .background { ${sprk_pos} }   /* positions mask */
+      .card:hover .background, .stars { ${sprk_pos} }   /* positions mask */
+      .card:hover .hair-color-fx { ${hue_rotate} }
+      .card:hover .jewels { ${gems_glow} }
       .black-hole, .dot { ${baxis_pos} }
+      .nebula { ${naxis_pos} }
       .card:hover + .border { ${tf_border} filter: drop-shadow(2px 4px 6px #ffd7002f) brightness(1.1); opacity: 1; text-shadow: 0px 0px 5px #ffd7009f;}
     `
     //var string = "x " + lp + " y " + tp;
